348626b07348a757eedd829745eac5326ee1e0db
Chris Najman <christopher.najman@gmail.com>
2025-02-01 14:20:08 +0000

    deleted unwanted js and html

--- set-and-toggle-attribute.js
+++
@@ -1,9 +0,0 @@
-export default function setAndToggleAttribute(el, attr, attrVal_1, attrVal_2) {
-  el.forEach((word) => {
-    let attribute = word.getAttribute(attr)
-    attribute =
-      attribute === attrVal_1
-        ? word.setAttribute(attr, attrVal_2)
-        : word.setAttribute(attr, attrVal_1)
-  })
-}
